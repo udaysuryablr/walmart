7bc079bc9983eacfe518626010b4e2ba922e2cd6
udaysuryablr <107272845+udaysuryablr@users.noreply.github.com>
2024-07-08 02:06:24 +0530

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -1,19 +1,1 @@
-// scripts.js
-window.addEventListener('load', () => {
-    const oceanSound = document.getElementById('ocean-sound');
-    const toggleButton = document.getElementById('toggle-music');
 
-    oceanSound.play().catch(error => {
-        console.log('Auto-play failed:', error);
-    });
-
-    toggleButton.addEventListener('click', () => {
-        if (oceanSound.paused) {
-            oceanSound.play();
-            toggleButton.innerHTML = '<i class="fas fa-volume-up"></i> Pause Music';
-        } else {
-            oceanSound.pause();
-            toggleButton.innerHTML = '<i class="fas fa-volume-mute"></i> Play Music';
-        }
-    });
-});
